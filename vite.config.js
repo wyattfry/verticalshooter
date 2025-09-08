9ff409f66aa80f0e0f99323cc337c121788cb948
Wyatt Fry <wyattfry@gmail.com>
2025-09-08 12:56:12 -0400

    wip

---
+++ vite.config.js
@@ -0,0 +1,14 @@
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+    build: {
+        rollupOptions: {
+            output: {
+                manualChunks(id) {
+                    if (id.includes('node_modules')) return 'vendor';
+                },
+            }
+        },
+        chunkSizeWarningLimit: 1000
+    }
+})
